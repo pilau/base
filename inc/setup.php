1e6c2a43bde638da8c79fbc13cabde33caa33b00
Steve Taylor <steve@sltaylor.co.uk>
2015-05-07 21:37:50 +0100

    Moved nav include to Starter

--- inc/setup.php
+++ inc/setup.php
@@ -47,39 +47,3 @@ if ( ! function_exists( 'pilau_remove_title_attributes' ) ) {
 		return preg_replace( '/\s*title\s*=\s*(["\']).*?\1/', '', $input );
 	}
 }
-
-
-if ( ! function_exists( 'pilau_nav_menu_css_classes' ) ) {
-	add_filter( 'nav_menu_item_id', '__return_empty_array', 10000 );
-	add_filter( 'nav_menu_css_class', 'pilau_nav_menu_css_classes', 10000, 3 );
-	/**
-	 * Remove unnecessary attributes from nav menu items
-	 * Note that this will remove any custom classes added in
-	 * the "CSS Classes (optional) field in nav menus
-	 * if they start with "menu-item"
-	 *
-	 * @link	http://codex.wordpress.org/Function_Reference/wp_nav_menu#Menu_Item_CSS_Classes
-	 * @since	Pilau_Base 0.2
-	 */
-	function pilau_nav_menu_css_classes( $classes, $item, $args ) {
-		$new_classes = array();
-		foreach ( $classes as $class ) {
-			// We're only keeping classes that indicate location, plus standard class
-			if ( $class == 'menu-item' || ( ! ( strlen( $class ) > 8 && substr( $class, 0, 9 ) == 'menu-item' ) && ( strpos( $class, 'page' ) === false || strpos( $class, 'ancestor' ) !== false ) ) ) {
-				$new_classes[] = $class;
-			}
-		}
-		return $new_classes;
-	}
-}
-
-
-if ( ! function_exists( 'default_nav_menu' ) ) {
-	/**
-	 * Blank default nav menu
-	 *
-	 * @link	http://www.rlmseo.com/blog/cutom-navigation-menus-in-wordpress-3-0/
-	 * @since	Pilau_Base 0.2
-	 */
-	function default_nav_menu() { return ''; }
-}
